f5818a42d2a4b4d67a1800dcfd584db6a2d497ff
Fiyin20 <142032833+Fiyingithub@users.noreply.github.com>
2025-07-18 23:43:41 +0100

    Update protected.js

--- middlewares/protected.js
+++ middlewares/protected.js
@@ -10,7 +10,7 @@ export const authenticate = (req, res, next) => {
   }
 
   const token = authHeader.split(' ')[1];
-
+  
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded; // this is where role is attached
